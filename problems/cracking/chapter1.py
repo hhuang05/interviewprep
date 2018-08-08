ce285fa639b5f1a60a53c7be7dbe5a8b151970c7
Moses Huang <hhuang05@gmail.com>
2018-08-08 17:17:23 -0400

    Updated is_one_away

--- problems/cracking/chapter1.py
+++ problems/cracking/chapter1.py
@@ -7,7 +7,7 @@ def is_one_away(s1, s2):
     else:
         str_a = s2
         str_b = s1
-
+        
     if (len(str_a) - 1 > len(str_b)): # more than 1 away
         return False
     
@@ -15,25 +15,36 @@ def is_one_away(s1, s2):
         diffs = 0
         for j in range(len(str_a)):
             if (str_a[j] != str_b[j]):
-                diff += 1
-                if (diff > 1):
+                diffs += 1
+                if (diffs > 1):
                     return False
         return True
     
     else: # one away, make sure no replace
-        diffs = j = 0
-        for i in range(len(str_b)):
+        diffs = i = j = 0
+        while(i < len(str_b)):
             if (str_b[i] != str_a[j]):
                 diffs += 1
                 if (diffs > 1):
                     return False
                 else:
-                    i -= 1
                     j += 1
             else:
                 j += 1
+                i += 1
+                
         return True
-        
+
+def test_oneaway():
+    print('Testing is_one_away() ...')
+    print('\'pale, ple\' is one away? {}'.format(is_one_away('pale', 'ple')))
+    print('\'pale, bale\' is one away? {}'.format(is_one_away('pale', 'bale')))
+    print('\'pale, bake\' is one away? {}'.format(is_one_away('pale', 'bake')))
+    print('\'pales, pale\' is one away? {}'.format(is_one_away('pales', 'pale')))
+    print('\'pales, pale\' is one away? {}'.format(is_one_away('pales', 'pale')))
+    print('\'pale, paile\' is one away? {}'.format(is_one_away('pale', 'paile')))
+    print('\'pales, paile\' is one away? {}'.format(is_one_away('pales', 'paile')))
+
 def is_unique(s):
     """ Returns true if string s only has unique characters, otherwise false """
     sorted_chars = sorted(s)
@@ -42,6 +53,10 @@ def is_unique(s):
             return False
     return True
 
+def test_isunique():
+    print('\'dog\' is unique? {}'.format(is_unique('dog')))
+    print('\'lassie\' is unique? {}'.format(is_unique('lassie')))
+    
 def is_permutation(s1, s2):
     if (len(s1) == len(s2)):
         s1_sorted = sorted(s1)
@@ -54,13 +69,12 @@ def is_permutation(s1, s2):
         return True
     else:
         return False
+
+def test_ispermutation():
+    print('\'waterbottle\' is a permutation of \'erbottlewat\'? {}'.format(is_permutation('waterbottle', 'erbottlewat')))
     
 def main():    
-    print('\'dog\' is unique? {}'.format(is_unique('dog')))
-    print('\'lassie\' is unique? {}'.format(is_unique('lassie')))
-    print('\'pale, ple\' is one away? {}'.format(is_one_away('pale', 'ple')))
-    print('\'pale, ple\' is one away? {}'.format(is_one_away('ple', 'pale')))
-    print('\'waterbottle\' is a permutation of \'erbottlewat\'? {}'.format(is_permutation('waterbottle', 'erbottlewat')))
+    test_oneaway()
     
 if __name__ == '__main__':
     main()
