cf7ba9d29f601a702017951e5da3563e21c322c7
Moses Huang <hhuang05@gmail.com>
2018-08-09 12:01:28 -0400

    Added 1.6

--- problems/cracking/chapter1.py
+++ problems/cracking/chapter1.py
@@ -1,5 +1,45 @@
 #! /usr/bin/python3
 
+def str_compress(s):
+    """ Attempts to compress string s by reduce repeats to a count
+
+    Examples::
+
+       'aabbcddeeaa' --> 'a2b2c1d2e2a2'
+    """
+    current_char = s[0]
+    output = []
+    count = 0
+    for c in s:        
+        if (c == current_char):
+            count += 1
+        else:
+            output.append(current_char)
+            output.append(str(count))
+            current_char = c
+            count = 1
+
+    # Have to account for the last char set which has been repeated
+    output.append(current_char)
+    output.append(str(count))
+    
+    no_comp = True
+    for i in range(1, len(output), 2):
+        if (output[i] != '1'):
+            no_comp = False
+            break
+
+    if (no_comp):
+        return s
+    else:
+        return ''.join(output)
+
+def test_strcompress():
+    print('Testing str_compress() ...')
+    print('Compressing aabbcddeeaabb to {}'.format(
+        str_compress('aabbcddeeaabb')))
+    print('Compressing abcdefg to {}'.format(str_compress('abcdefg')))
+    
 def is_one_away(s1, s2):
     if (len(s1) > len(s2)):
         str_a = s1
@@ -74,7 +114,7 @@ def test_ispermutation():
     print('\'waterbottle\' is a permutation of \'erbottlewat\'? {}'.format(is_permutation('waterbottle', 'erbottlewat')))
     
 def main():    
-    test_oneaway()
+    test_strcompress()
     
 if __name__ == '__main__':
     main()
