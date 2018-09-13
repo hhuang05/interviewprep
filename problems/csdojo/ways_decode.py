d7919a723ebe1446862de0f8fcd15345a9be84cf
Moses Huang <hhuang05@gmail.com>
2018-09-13 11:44:43 -0400

    Updated ways to decode

--- problems/csdojo/ways_decode.py
+++ problems/csdojo/ways_decode.py
@@ -9,16 +9,21 @@ def decode(input_s, result):
     """ decode :: string -> list -> list """
     if (len(input_s) == 0):  #consumed input
         return result
-    elif (len(input_s) == 1): 
-        return decode('', list(map(lambda x: x + dl_map.get(input_s), result)))
+    elif (len(input_s) == 1):
+        if (dl_map.get(input_s) is not None):
+            return decode('', list(map(lambda x: x + dl_map.get(input_s), result)))
+        else:
+            return result
     elif (len(input_s) == 2):
         if (dl_map.get(input_s) is not None):
 	    # result is processing string length of 2 and then also process string of 1
             Len2_r = decode('', list(map(lambda x: x + dl_map.get(input_s), result)))
             Len1_r = decode(input_s[1:], list(map(lambda x: x + dl_map.get(input_s[0]), result)))
             return Len2_r + Len1_r
-        else:
+        elif (dl_map.get(input_s[0]) is not None):
             return decode(input_s[1:], list(map(lambda x: x + dl_map.get(input_s[0]), result)))
+        else:
+            return result
     else:
         R1 = decode(input_s[1:], list(map(lambda x: x + dl_map.get(input_s[0]), result)))
         R2 = decode(input_s[2:], list(map(lambda x: x + dl_map.get(input_s[:2]), result)))
@@ -26,10 +31,12 @@ def decode(input_s, result):
 
 def main():
     print(decode('', ['']))
+    print(decode('01', ['']))
     print(decode('1', ['']))
     print(decode('12', ['']))
     print(decode('121', ['']))
     print(decode('12126', ['']))
+    print(decode('1111111111111', ['']))
     
 if __name__ == '__main__':
     main()
